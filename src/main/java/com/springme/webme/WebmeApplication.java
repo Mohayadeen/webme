decebd29abff76a31c116edb631434c22023c05d
Mohayadeen <mabdulk8@for.com>
2022-12-22 19:45:02 +0530

    rest api

---
+++ src/main/java/com/springme/webme/WebmeApplication.java
@@ -0,0 +1,22 @@
+package com.springme.webme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmeApplication.class, args);
+	}
+
+	@RequestMapping("/")
+    public String home() 
+	{
+    	return "Hello Docker World";
+    }
+
+}
